527b426d4e38645db72fd0f696bc868e8ef7af18
AxoltDash <darshan@ciencias.unam.mx>
2025-03-07 16:49:23 -0600

    :rocket: Added haskell language server

--- pkgs/languages.nix
+++ pkgs/languages.nix
@@ -5,7 +5,7 @@
   environment.systemPackages = with pkgs; [
     # HASKELL =--------------------------------=
     ghc
-    # haskell-language-server
+    haskell-language-server
     # Libraries
     cabal-install
     # haskellPackages.minisat-solver
