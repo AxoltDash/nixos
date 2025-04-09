350888f785e3a48bedb05f879c637dc6f9654d95
AxoltDash <darshan@ciencias.unam.mx>
2025-04-09 12:15:44 -0600

    :pencil2: Fix ; nixosLabel

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
   # CHAGE LABEL! #
   ################
   # =======================
-  system.nixos.label = "ZOOM-Metings;
+  system.nixos.label = "ZOOM-Metings";
   # =======================
 
   imports =
