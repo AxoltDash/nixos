5c643ce730432396e4b7ef09c35147eef37c768a
AxoltDash <darshan@ciencias.unam.mx>
2025-05-06 13:16:16 -0600

    :lipstick: Fix the indexation

--- home/home.nix
+++ home/home.nix
@@ -1,136 +1,136 @@
 { system, inputs, config, options, lib, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "axolt";
-  home.homeDirectory = "/home/axolt";
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = "axolt";
+    home.homeDirectory = "/home/axolt";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    inputs.zen-browser.packages."${system}".default
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    home.packages = with pkgs; [
+        inputs.zen-browser.packages."${system}".default
+        # # Adds the 'hello' command to your environment. It prints a friendly
+        # # "Hello, world!" when run.
+        # pkgs.hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+        # # It is sometimes useful to fine-tune packages, for example, by applying
+        # # overrides. You can do that directly here, just don't forget the
+        # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+        # # fonts?
+        # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+        # # You can also create simple shell scripts directly inside your
+        # # configuration. For example, this adds a command 'my-hello' to your
+        # # environment:
+        # (pkgs.writeShellScriptBin "my-hello" ''
+        #   echo "Hello, ${config.home.username}!"
+        # '')
+    ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = {
+        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+        # # symlink to the Nix store copy.
+        # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/axolt/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-  };
-
-  # GIT =-------------------------------------------------=
-  programs.git = {
-    enable = true;
-    userName = "AxoltDash";
-    userEmail = "darshan@ciencias.unam.mx";
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
-
-  programs.ssh = {
-    enable = true;
-    matchBlocks = {
-      "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519";
-      };
-    };
-  };   
-
-  # THEMES =------------------------------------------------=
-  gtk = {
-    enable = true;
-    theme = {
-      package = pkgs.gruvbox-material-gtk-theme;
-      name = "Gruvbox-Material-Dark";
-    };
-    iconTheme = {
-      package = pkgs.gruvbox-material-gtk-theme;
-      name = "Gruvbox-Material-Dark"; 
+        # # You can also set the file content immediately.
+        # ".gradle/gradle.properties".text = ''
+        #   org.gradle.console=verbose
+        #   org.gradle.daemon.idletimeout=3600000
+        # '';
     };
 
-    #iconTheme = {
-    #  package = pkgs.gruvbox-plus-icons;
-    #  name = "Gruvbox-Plus-Dark"; 
-    #};
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
-    style.name = "adwaita-dark";
-    style.package = pkgs.adwaita-qt;
-  };
-
-  home.pointerCursor = {
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 12;
-    hyprcursor = {
-      enable = true;
-      size = 12;
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/axolt/etc/profile.d/hm-session-vars.sh
+    #
+    home.sessionVariables = {
+        EDITOR = "nvim";
+        VISUAL = "nvim";
     };
-    x11.enable = true;
-    gtk.enable = true;
-  };
-  
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+
+    # GIT =-------------------------------------------------=
+    programs.git = {
+        enable = true;
+        userName = "AxoltDash";
+        userEmail = "darshan@ciencias.unam.mx";
+        extraConfig = {
+            init.defaultBranch = "main";
+        };
+    };
+
+    programs.ssh = {
+        enable = true;
+        matchBlocks = {
+            "github.com" = {
+                hostname = "github.com";
+                user = "git";
+                identityFile = "~/.ssh/id_ed25519";
+            };
+        };
+    };   
+
+    # THEMES =------------------------------------------------=
+    gtk = {
+        enable = true;
+        theme = {
+            package = pkgs.gruvbox-material-gtk-theme;
+            name = "Gruvbox-Material-Dark";
+        };
+        iconTheme = {
+            package = pkgs.gruvbox-material-gtk-theme;
+            name = "Gruvbox-Material-Dark"; 
+        };
+
+        #iconTheme = {
+        #  package = pkgs.gruvbox-plus-icons;
+        #  name = "Gruvbox-Plus-Dark"; 
+        #};
+    };
+
+    qt = {
+        enable = true;
+        platformTheme.name = "gtk";
+        style.name = "adwaita-dark";
+        style.package = pkgs.adwaita-qt;
+    };
+
+    home.pointerCursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 12;
+        hyprcursor = {
+            enable = true;
+            size = 12;
+        };
+        x11.enable = true;
+        gtk.enable = true;
+    };
+    
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
 }
--- pkgs/gpu-cpu.nix
+++ pkgs/gpu-cpu.nix
@@ -1,38 +1,37 @@
 { config, pkgs, ... }:
 
 {
-  # 🔥 Habilitar el driver AMDGPU (gráficos integrados)
-  # services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = with pkgs; [ amdvlk ];
-      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    # 🔥 Habilitar el driver AMDGPU (gráficos integrados)
+    # services.xserver.videoDrivers = [ "amdgpu" ];
+    hardware = {
+        graphics = {
+            enable = true;
+            enable32Bit = true;
+            extraPackages = with pkgs; [ amdvlk ];
+            extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+        };
+        amdgpu.amdvlk = {
+            enable = true;
+            support32Bit.enable = true;
+        };
     };
-    amdgpu.amdvlk = {
-      enable = true;
-      support32Bit.enable = true;
-    };
-  };
 
-  # 🖥️ Mesa y Vulkan para mejor compatibilidad gráfica
-  environment.systemPackages = with pkgs; [
-    mesa  # Drivers OpenGL/Vulkan
-    mesa.opencl
-    mesa-demos  # Herramientas para probar OpenGL
-    vulkan-tools  # Herramientas Vulkan
-    vulkan-validation-layers
-    # blender-hip
-  ];
-  
-  # 💻 Cpu
-  # hardware.cpu.amd.updateMicrocode = true;
+    # 🖥️ Mesa y Vulkan para mejor compatibilidad gráfica
+    environment.systemPackages = with pkgs; [
+        mesa  # Drivers OpenGL/Vulkan
+        mesa.opencl
+        mesa-demos  # Herramientas para probar OpenGL
+        vulkan-tools  # Herramientas Vulkan
+        vulkan-validation-layers
+        # blender-hip
+    ];
+    
+    # 💻 Cpu
+    # hardware.cpu.amd.updateMicrocode = true;
 
-  # 🔋 Manejo de bateria
-  powerManagement.enable = true;
-  services.tlp.enable = true;
+    # 🔋 Manejo de bateria
+    powerManagement.enable = true;
+    services.tlp.enable = true;
 
-  services.xserver.videoDrivers = [ "amdgpu" ];
+    services.xserver.videoDrivers = [ "amdgpu" ];
 }
-
