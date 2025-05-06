aaed7285b25241d0d7407f4002a2156d729b5116
AxoltDash <darshan@ciencias.unam.mx>
2025-05-06 13:18:05 -0600

    :lipstick: Change name for derivation

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     # CHAGE LABEL! #
     ################
     # =======================
-    system.nixos.label = "lua51-tiktoken";
+    system.nixos.label = "lipstick";
     # =======================
 
     imports =
