bc2fc12a3e6d3e24472b8097086e0034216a395d
AxoltDash <darshan@ciencias.unam.mx>
2025-05-08 10:11:42 -0600

    :pencil2: Fix Nixos Label to: trim-generations

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     # CHAGE LABEL! #
     ################
     # =======================
-    system.nixos.label = "Hyprshade";
+    system.nixos.label = "Trim-generations";
     # =======================
 
     imports =
