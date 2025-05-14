8ecf6aeeda9d4ea6446dc3ac6717f049cecd1c3f
AxoltDash <darshan@ciencias.unam.mx>
2025-05-13 20:13:33 -0600

    :truck: Move Bluetooth congif to hardware config

--- configuration.nix
+++ configuration.nix
@@ -110,33 +110,6 @@
         STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/axolt/.steam/root/compatibilitytools.d";
     };
 
-    # Enable Bluetooth
-    hardware.bluetooth = {
-        enable = true;
-        powerOnBoot = true;
-        settings.General = {
-            experimental = true; # show battery
-
-            # https://www.reddit.com/r/NixOS/comments/1ch5d2p/comment/lkbabax/
-            # for pairing bluetooth controller
-            Privacy = "device";
-            JustWorksRepairing = "always";
-            Class = "0x000100";
-            FastConnectable = true;
-        };
-    };
-    services.blueman.enable = true; 
-    hardware.xpadneo.enable = true; # Enable the xpadneo driver for Xbox One wireless controllers
-
-    # Botting
-    boot = {
-        extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
-        extraModprobeConfig = ''
-            options bluetooth disable_ertm=Y
-        '';
-        # connect xbox controller
-    };
-    
     # Fonts 
     fonts.packages = with pkgs; [
         nerd-fonts.jetbrains-mono
