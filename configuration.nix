8b52cabdf910e08883367c92d0fafc313e41eeac
AxoltDash <darshan@ciencias.unam.mx>
2025-06-29 09:15:03 -0600

    :packages: 2025-06-29 Update

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     # CHAGE LABEL! #
     ################
     # =======================
-    system.nixos.label = "Thunar";
+    system.nixos.label = "UPDATE";
     # =======================
 
     imports =
