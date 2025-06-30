6ac2a6b4d75b5e0649389f5ab056a8d57e1cfba9
AxoltDash <darshan@ciencias.unam.mx>
2025-06-30 00:57:29 -0600

    :pencil2: Fixed Nixos Label

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     # CHAGE LABEL! #
     ################
     # =======================
-    system.nixos.label = "Brave-Browser-Wayland";
+    system.nixos.label = "Wayland-OZONE";
     # =======================
 
     imports =
