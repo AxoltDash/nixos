38b225db72afb3baec18abc53f1dbf38d38a84fb
AxoltDash <darshan@ciencias.unam.mx>
2025-07-21 17:04:07 -0600

    :bug: change pkg branch to stable verision
    I changed the branc from unestable to 25-05 (stable), this because i
    have a issue with java8

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
     inputs = {
         # Nixpkgs: Repositorio de paquetes
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
         # Home Manager
         home-manager = {
-            url = "github:nix-community/home-manager";
+			# url = "github:nix-community/home-manager";
+			url = "github:nix-community/home-manager/release-25.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
