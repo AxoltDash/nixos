e86e34af45d24edf1eef3eb335981413274632ac
AxoltDash <darshan@ciencias.unam.mx>
2026-01-04 17:34:47 -0600

    :bug: removed driversi686Linux.amdvlk from extra packages for 32bits amd systems

--- pkgs/hardware.nix
+++ pkgs/hardware.nix
@@ -7,7 +7,7 @@
             enable = true;
             enable32Bit = true;
             # extraPackages = with pkgs; [ amdvlk ];
-            extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+			# extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
         };
         # amdgpu.amdvlk = {
         #     enable = true;
