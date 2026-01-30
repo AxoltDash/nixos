ad55ae5e10be7abd816b3e5cd489432479e87d6e
AxoltDash <darshan@ciencias.unam.mx>
2026-01-30 15:49:09 -0600

    :rocket: Upower management added

--- pkgs/hardware.nix
+++ pkgs/hardware.nix
@@ -36,6 +36,14 @@
 		};
 	};
 
+	services.upower = {
+		enable = true;
+		percentageLow = 20;
+		percentageCritical = 10;
+		percentageAction = 5;
+		criticalPowerAction = "PowerOff";  # Solo apagar cuando llegue a 5%
+	};
+
 
     # BLUETOOTH =-------------------------=
 
