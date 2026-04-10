33c22dd2bf191456e94fbdb2b002fa45b8e29aa5
AxoltDash <darshan@ciencias.unam.mx>
2026-04-10 16:42:17 -0600

    :rocket: pynvim

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     # CHAGE LABEL! #
     ################
     # =======================
-    system.nixos.label = "Unstable-Branch-2";
+    system.nixos.label = "pynvim";
     # ======================
 
     imports =
